fa4d377ef24633c2ef9da15a61ea4d7389c7d5f6
Andres Alonso Forero Pacheco <aforeroa@uninorte.edu.co>
2024-05-10 17:25:41 -0500

    Feat: Modify main.py and created data().py

---
+++ data().py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 10 17:23:02 2024
+
+@author: andre
+"""
+
+{"data":59}
--- pgit0.py
+++ pgit0.py
@@ -7,6 +7,8 @@ Created on Fri May 10 16:17:14 2024
 
 def main() -> None:
     print("Hello World")
+    a,b = 50,9
+    print("Suma =", a+b)
 
 if __name__ == "__main__":
     main()
